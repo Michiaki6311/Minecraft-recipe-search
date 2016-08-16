4ba31a970634aa2ba99f7e5f2c8739c68dde16c9
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-16 02:45:07 +0000

    add lingr response

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ get '/' do
   'Hello,World!'
 end
 
-get '/search' do
+post '/search' do
   url = "http://www26.atwiki.jp/minecraft/pages/1073.html"
   
   doc = Nokogiri::HTML.parse(open(url), nil,"utf-8")
@@ -30,10 +30,20 @@ get '/search' do
      image.push(item_img_new)
    end
    
- count = name.index{|item|item =~ /^かまど/}
- 
- puts name[count]
- puts craft[count]
- puts image[count]
+    searchword = ""
+  j = JSON.parse(request.body.string)
+  j['events'].select{|e| e['message']}.map{|e|
+    if e['message']['text'] =~ /^#mr/ then
+      searchword = e['message']['text'].gsub(/^#mr\s/,'')
+    else
+      searchword = ""
+    end
+  }
+    count = name.index{|item|item =~ /#{searchword}/}
+    
+    puts name[count]
+    puts craft[count]
+    puts image[count]
+
 end
 
