01004de0c9114b12697727d2ba304cfa1f17f36c
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-16 04:21:07 +0000

    fix indent

--- app.rb
+++ app.rb
@@ -5,45 +5,45 @@ require 'uri'
 require 'nokogiri'
 
 get '/' do
-  'Hello,World!'
+'Hello,World!'
 end
 
 post '/search' do
-    searchword = ""
-  j = JSON.parse(request.body.string)
-  j['events'].select{|e| e['message']}.map{|e|
-    if e['message']['text'] =~ /^#mr/ then
-      searchword = e['message']['text'].gsub(/^#mr\s/,'')
-      
-    url = "http://www26.atwiki.jp/minecraft/pages/1073.html"
-  
-  doc = Nokogiri::HTML.parse(open(url), nil,"utf-8")
-  
-  name = []
-  craft = []
-  image = []
-  
-   doc.xpath("//tr//td[@style='text-align:center;vertical-align:MIDDLE;background-color:lightblue;']").each do |item_name|
-     name.push(item_name.text)
-   end
-    
-   doc.xpath("//tr//td[@style='vertical-align:MIDDLE;']").each do |item_craft|
-     craft.push(item_craft.text)
-   end
-   
-   doc.xpath("//tr//td[@style='text-align:center;vertical-align:MIDDLE;background-color:lightgreen;']").each do |item_img|
-     item_img_new = "http:#{item_img.css('img').attribute('src').value}"
-     image.push(item_img_new)
-   end
-   
-   count = name.index{|item|item =~ /#{searchword}/}
-    
-  "#{name[count]}\n#{craft[count]}\n#{image[count]}\n"
- 
-    else
-      searchword = ""
-    end
-  }
+	searchword = ""
+j = JSON.parse(request.body.string)
+	j['events'].select{|e| e['message']}.map{|e|
+		if e['message']['text'] =~ /^#mr/ then
+			searchword = e['message']['text'].gsub(/^#mr\s/,'')
+
+				url = "http://www26.atwiki.jp/minecraft/pages/1073.html"
+
+				doc = Nokogiri::HTML.parse(open(url), nil,"utf-8")
+
+				name = []
+				craft = []
+				image = []
+
+				doc.xpath("//tr//td[@style='text-align:center;vertical-align:MIDDLE;background-color:lightblue;']").each do |item_name|
+				name.push(item_name.text)
+				end
+
+				doc.xpath("//tr//td[@style='vertical-align:MIDDLE;']").each do |item_craft|
+				craft.push(item_craft.text)
+				end
+
+				doc.xpath("//tr//td[@style='text-align:center;vertical-align:MIDDLE;background-color:lightgreen;']").each do |item_img|
+				item_img_new = "http:#{item_img.css('img').attribute('src').value}"
+				image.push(item_img_new)
+				end
+
+				count = name.index{|item|item =~ /#{searchword}/}
+
+		"#{name[count]}\n#{craft[count]}\n#{image[count]}\n"
+
+		else
+			searchword = ""
+				end
+	}
 
 end
 
